338a0bc602f18dc016eff31cb3ca6311360c6850
amyzhao3969 <zhao_yiming@hotmail.com>
2021-03-08 14:54:16 -0500

    Add invalid date API endpoint

--- server.js
+++ server.js
@@ -21,10 +21,14 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
+  console.log({greeting: "hello API"});
   res.json({greeting: 'hello API'});
 });
 
-
+app.get("/api/timestamp/:date_string", function(req, res) {
+  console.log(req, " <= req");
+  res.json({"error": "Invalid Date"});
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
